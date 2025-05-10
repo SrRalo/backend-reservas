9adb05a23272688c85f02676ad853d8ff32a721d
SrRalo <adreloa@gmail.com>
2025-05-09 22:34:23 -0500

    first commit

---
+++ app/Http/Controllers/PenalizacionController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Models\Penalizacion;
+use Illuminate\Http\Request;
+
+class PenalizacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Penalizacion $penalizacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Penalizacion $penalizacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Penalizacion $penalizacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Penalizacion $penalizacion)
+    {
+        //
+    }
+}
